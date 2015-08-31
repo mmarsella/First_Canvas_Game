69d7656def8c748598c8a0f5882dbc6f0aec56f9
Mark Marsella <mmarsella114@g.rwu.edu>
2015-08-31 08:48:33 -0700

    collision detection + randomBallColor

--- game.js
+++ game.js
@@ -4,11 +4,16 @@
 // https://developer.mozilla.org/en-US/docs/Games/Workflows/2D_Breakout_game_pure_JavaScript/Move_the_ball
 
 
+
 // storing a reference to the canvas 
 var canvas = document.getElementById("myCanvas");
 
 // stores all 2d rendering
 var ctx = canvas.getContext("2d");
+
+
+//Color of the ball
+var color = "#0095DD";
 
 
 
@@ -29,6 +34,8 @@ var y = canvas.height-30;
 var dx = -1;
 var dy = -2;
 
+var ballRadius = 10;
+
 function draw()
 {
   /** MAKING IT MOVE */
@@ -38,6 +45,28 @@ function draw()
  
  drawBall();
 
+/** COLLISION DETECTION */
+ // ball bouncing off top edge
+// If the y value of the ball is lower than zero, change the direction of the y axis by setting it = to itself.. reversed.
+ // If the ball was moving upwards with a speed of 2px/fm, set to moving down by 2px (-2)
+ if(y + dy < ballRadius)  // calcs collision point of wall and ball's radius
+ {                        // prevents ball from going halfway into the wall
+   dy = -dy;
+   color = randomColor();
+ }
+ //ball bouncing off bottom edge
+ if(y + dy > canvas.height - ballRadius){
+  dy = -dy;
+  color = randomColor();
+
+ }
+
+// ballRadius replaces 0
+if(x + dx < ballRadius || x+dx > canvas.width - ballRadius){
+  dx = -dx;
+  color = randomColor();
+
+}
   // update x and y with our dx and dy variable on every frame
   x += dx;
   y += dy;
@@ -48,14 +77,25 @@ function drawBall()
    // drawing code
   ctx.beginPath();
   //why the Math.PI * 2???
-  ctx.arc(x,y,10,0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.arc(x,y,ballRadius,0, Math.PI*2);
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
 
 
 setInterval(draw, 10);  //calls draw() every 10ms
+
+function randomColor()
+{
+var randomOne = Math.floor(Math.random() * 355) + 50;
+var randomTwo = Math.floor(Math.random() * 355) + 50;
+var randomThree = Math.floor(Math.random() * 355) + 50;
+
+var color = "rgb("+randomOne+","+randomTwo+","+randomThree+")";
+
+return color;
+}
 
 
 
